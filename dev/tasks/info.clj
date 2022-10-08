b7045581fddd083aa337b7e307a7ec6356268c23
Ferdinand Beyer <fb@fbeyer.com>
2022-10-08 12:48:27 +0200

    Improve version tooling

--- dev/tasks/info.clj
+++ dev/tasks/info.clj
@@ -2,9 +2,3 @@
 
 (def github-url "https://github.com/ferdinand-beyer/vscode-babashka-tasks")
 (def version "0.1.0")
-
-(def tag (str "v" version))
-
-(defn github-actions []
-  (println (str "::set-output name=version::" version))
-  (println (str "::set-output name=tag::" tag)))
--- dev/tasks/version.clj
+++ dev/tasks/version.clj
@@ -1,15 +1,15 @@
 (ns tasks.version
-  (:require [clojure.string :as str]
-            [cheshire.core :as json]
+  (:require [babashka.fs :as fs]
+            [clojure.string :as str]
+            [semver.core :as semver]
             [tasks.info :as info]
-            [tasks.tools :refer [git]])
+            [tasks.tools :refer [*cwd* git npm]])
   (:import [java.time LocalDateTime]
            [java.time.format DateTimeFormatter]))
 
 (defn- current-version []
-  (-> (slurp "package.json")
-      (json/parse-string true)
-      :version))
+  (require :reload '[tasks.info :as info])
+  @(resolve 'info/version))
 
 (defn- today []
   (.format (DateTimeFormatter/ofPattern "yyyy-MM-dd")
@@ -25,33 +25,68 @@
                         "[" next-version "]: " info/github-url "/compare/v"
                         current-version "...v" next-version "\n"))))
 
-(defn- changelog [current-version next-version]
+(defmulti ^:private update-file
+  (fn [filepath _current-version _next-version]
+    (fs/file-name filepath)))
+
+(defmethod update-file :default
+  [filepath current-version next-version]
+  (-> (slurp filepath)
+      (str/replace current-version next-version)
+      (->> (spit filepath))))
+
+(defmethod update-file "CHANGELOG.md"
+  [filepath current-version next-version]
   (let [content (slurp "CHANGELOG.md")]
     (when-not (str/includes? content (str "[" next-version "]"))
-      {"CHANGELOG.md" (update-changelog content current-version next-version (today))})))
+      (spit filepath (update-changelog content current-version next-version (today))))))
 
-(def files
-  ["README.md"
+(defmethod update-file "package.json"
+  [filepath _ next-version]
+  (let [dir (some-> (fs/parent filepath) str)]
+    (binding [*cwd* (when (seq dir) dir)]
+      (npm :version next-version "--git-tag-version" "false"))))
+
+(def ^:private files
+  ["dev/tasks/info.clj"
+   "CHANGELOG.md"
+   "README.md"
    "package.json"])
 
-(defn- version-updates [next-version]
-  (let [current-version (current-version)]
-    (merge
-     (changelog current-version next-version)
-     (zipmap
-      files
-      (for [file files]
-        (str/replace (slurp file) current-version next-version))))))
+(defn- swap-version [current-version next-version]
+  (run! #(update-file % current-version next-version) files))
 
-(defn- set-version [version]
-  (doseq [[file-name content] (version-updates version)]
-    (spit file-name content)))
+(defn- set-version [next-version]
+  (swap-version (current-version) next-version))
+
+(defn- transform [f]
+  (let [current (current-version)]
+    (swap-version current (semver/transform f current))))
+
+(defn major []
+  (transform semver/increment-major))
+
+(defn minor []
+  (transform semver/increment-minor))
+
+(defn patch []
+  (transform semver/increment-patch))
+
+(defn version [s]
+  (case (name s)
+    "major" (major)
+    "minor" (minor)
+    "patch" (patch)
+    (set-version (str/replace s #"^v" ""))))
 
 (defn tag []
-  (set-version info/version)
-  (git :add "-u")
-  (git :commit "-m" (str "Release " info/tag))
-  (git :tag info/tag))
+  (let [tag (str "v" (current-version))
+        msg (str "Release " tag)]
+    (git :add "-u")
+    (git :commit "-m" msg)
+    (git :tag "-a" "-m" msg tag)))
 
-(defn -main [version]
-  (set-version version))
+(defn -main [& [s]]
+  (if (some? s)
+    (version s)
+    (println (current-version))))
